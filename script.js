0379b5571c652bd3114a68fb1a5299eff65aab0f
Armaan Ali <armanbhatti276@gmail.com>
2024-08-15 05:10:01 +0500

    Update Card

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-$(".colour-value span").click(function () {
-    $(".colour-value span").removeClass("active");
-    $(this).addClass("active");
-    $("body").css("background", $(this).attr("data-color"));
-    $(".wrapper .price").css("color", $(this).attr("data-color"));
-    $(".size-value span.color").css("color", $(this).attr("data-color"));
-    $(".size-value span.active").css("background", $(this).attr("data-color"));
-    $(".image img").attr("src", $(this).attr("data-img"));
-    $(".btns button").css({
-        "background": $(this).attr("data-color"),
-        "border-color": $(this).attr("data-color")
-    });
-});
+document.addEventListener('DOMContentLoaded', function() {
+   let colorEle = document.querySelectorAll('.colour-value span');
+   let sizeEle = document.querySelectorAll(".size-value span")
+   for(let el of colorEle) {
+        el.addEventListener('click', function() {
+            for(let el of colorEle) {
+                el.classList.remove('active')
+            }
+            this.classList.add('active')
+            let getColor = this.dataset.color
+            let getImage = this.dataset.img
+            document.body.style.background = getColor;
+            document.querySelector(".wrapper .price").style.color = getColor;
+            document.querySelectorAll(".size-value span.color").forEach(colorE => {
+                colorE.style.color = getColor;
+            })
+            document.querySelector(".size-value span.active").style.background = getColor;
+            document.querySelector(".image img").setAttribute('src', getImage)
+            document.querySelectorAll(".btns button").forEach(btn => {
+                btn.style.background = getColor
+                btn.style.borderColor = getColor
+            })
+       })
+   }
+   for(let colorE of sizeEle) {
+    colorE.addEventListener('click', function() {
+        let bgColor = document.querySelector('.colour-value span.active').dataset.color
+        for(let el of sizeEle) {
+            el.classList.remove('active')
+            el.classList.add('color')
+            el.style.background = '#fff'
+            el.style.color = bgColor
+        }
+        this.style.background = bgColor
+        this.style.color = '#fff'
+        this.classList.add('active')
+        this.classList.remove('color')
+    })
+   }
+})
